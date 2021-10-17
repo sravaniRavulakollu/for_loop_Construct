eb1f54e67c6c40485e0aa8e70d0364f7827b272c
sravaniRavulakollu <87975506+sravaniRavulakollu@users.noreply.github.com>
2021-10-17 22:04:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace For_Loop__Construct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your name");
+            string Name = Console.ReadLine();
+
+            Console.Write("How many times you want to display your name");
+            int Count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(Name);
+            }
+        }
+    }
+}
